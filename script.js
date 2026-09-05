a6918642f2e9eb71eaad8b5c32c677eda8e0dcc5
vaishnavi06132 <vsshankrit@gmail.com>
2026-09-05 23:34:35 +0530

    some animation added

--- script.js
+++ script.js
@@ -69,3 +69,44 @@ class Particle
         }
     }
 }
+function handleParticles() 
+{
+    for (let i=0;i<pA.length;i++) 
+    {
+        pA[i].update();
+        pA[i].draw();
+        if (pA[i].size <= 0.2) 
+        {
+            pA.splice(i, 1);
+            i--;
+        }
+    }
+}
+function Holimsg() 
+{
+    if (txtOp<=0) 
+        return;
+    ctx.save();
+    ctx.globalAlpha = txtOp;
+    ctx.textAlign = 'center';
+    ctx.font = 'bold 70px Arial';
+    ctx.fillStyle = '#dd7eae';
+    ctx.fillText('HAPPY HOLI', canvas.width/2, canvas.height/2-90);
+    ctx.font = 'bold 50px Arial';
+    ctx.fillStyle = '#ff3366';
+    ctx.fillText('Spread the Joy!', canvas.width/2, canvas.height/2-20);
+    ctx.font = '20px Arial';
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText('Click or move your cursor to enjoy the colors', canvas.width/2, canvas.height/2+30);
+    ctx.restore();
+}
+function animate() 
+{
+    ctx.fillStyle = 'rgba(17, 17, 17, 0.1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    Holimsg();
+    handleParticles();
+    hue += 5;
+    requestAnimationFrame(animate);
+}
+animate();
