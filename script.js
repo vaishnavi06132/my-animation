b5c9f55f2752c7e331ab6d9bc231a8a490a27f00
vaishnavi06132 <vsshankrit@gmail.com>
2026-09-05 22:00:34 +0530

    added click and mousemove animation

--- script.js
+++ script.js
@@ -9,7 +9,25 @@ const mouse={
     x: undefined,
     y: undefined
 };
-window.addEventListener('resize', function () {
+window.addEventListener('resize', function(){
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+});
+window.addEventListener('mousemove', function(event)
+{
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for(let i=0;i<35;i++) 
+    {
+        particlesArray.push(new Particle());
+    }
+});
+window.addEventListener('click', function(event)
+{
+    mouse.x = event.x;
+    mouse.y = event.y;
+    for(let i=0;i<70;i++) 
+    {
+        particlesArray.push(new Particle());
+    }
 });
